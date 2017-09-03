40c40ae31ccd55039253781cb96f904b5961237a
Dongmin An <21600397@handong.edu>
2017-09-04 00:02:10 +0900

    added previous files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,19 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { LivePage } from '../pages/live/live';
+import { ListPage } from '../pages/list/list';
+import { SettingsPage } from '../pages/settings/settings';
+
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    LivePage,
+    ListPage,
+    SettingsPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +27,10 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    LivePage,
+    ListPage,
+    SettingsPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LivePage } from '../live/live';
+import { ListPage } from '../list/list';
+import { SettingsPage } from '../settings/settings';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	constructor(public navCtrl: NavController) {
 
-  }
+	}
 
+	launchLivePage() {
+		this.navCtrl.push(LivePage)
+	}
+
+	launchListPage() {
+		this.navCtrl.push(ListPage)
+	}
+
+	launchSettingsPage() {
+		this.navCtrl.push(SettingsPage)
+	}
 }
